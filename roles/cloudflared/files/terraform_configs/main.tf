6585945aae00d8b43d99cb5d5071de6e06f3cb0f
Timmo <66395069+timmo-d@users.noreply.github.com>
2025-09-16 21:51:23 +0800

    sdfg

--- roles/cloudflared/files/terraform_configs/main.tf
+++ roles/cloudflared/files/terraform_configs/main.tf
@@ -12,9 +12,32 @@ provider "cloudflare" {
   api_key = var.cloudflare_global_api_key
 }
 
-resource "cloudflare_origin_ca_certificate" "origin_cert" {
-  csr                = file("${path.module}/origin.csr")
-  hostnames          = var.cloudflare_hostnames
-  requested_validity = var.cert_validity_days
-  request_type       = "origin-rsa"
+# 1. Create the tunnel
+resource "cloudflare_zero_trust_tunnel_cloudflared" "home_edge" {
+  account_id = var.cloudflare_account_id
+  name       = "home-edge"
+  secret     = random_password.tunnel_secret.result
+}
+
+# 2. Generate a strong secret for the tunnel
+resource "random_password" "tunnel_secret" {
+  length  = 64
+  special = false
+}
+
+# 3. Create DNS route for the tunnel
+resource "cloudflare_zero_trust_tunnel_route" "home_edge_route" {
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.home_edge.id
+  network    = "0.0.0.0/0"
+}
+
+# 4. Output the token JSON
+output "tunnel_credentials_json" {
+  value     = jsonencode({
+    AccountTag   = var.cloudflare_account_id
+    TunnelSecret = base64encode(random_password.tunnel_secret.result)
+    TunnelID     = cloudflare_zero_trust_tunnel_cloudflared.home_edge.id
+  })
+  sensitive = true
 }
