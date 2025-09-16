5db968eb4c33a61af7b7aabb56742755e7baf33d
Timmo <66395069+timmo-d@users.noreply.github.com>
2025-09-16 22:47:56 +0800

    ZXc

--- roles/cloudflared/files/terraform/variables.tf
+++ roles/cloudflared/files/terraform/variables.tf
@@ -1,36 +1,10 @@
-variable "cloudflare_email" {
+variable "cloudflare_api_token" {
   type        = string
-  description = "Cloudflare account email"
-}
-
-variable "cloudflare_global_api_key" {
-  type        = string
-  description = "Cloudflare global API key"
-}
-
-variable "cloudflare_hostnames" {
-  type        = list(string)
-  description = "List of hostnames for the Origin CA certificate"
-}
-
-variable "cert_validity_days" {
-  type        = number
-  default     = 5475
-  description = "Certificate validity period in days"
+  sensitive   = true
 }
 
 variable "cloudflare_account_id" {
   type        = string
-  description = "Cloudflare account ID"
-}
-
-
-
-
-
-variable "cloudflare_api_token" {
-  type        = string
-  sensitive   = true
 }
 
 variable "tunnel_name" {
