5413e2224d1e717056e3bc99de6cdb5afe7456b0
Timmo <66395069+timmo-d@users.noreply.github.com>
2025-09-16 20:35:49 +0800

    zsdgf

--- roles/cloudflared/files/terraform_configs/main.tf
+++ roles/cloudflared/files/terraform_configs/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_global_api_key
